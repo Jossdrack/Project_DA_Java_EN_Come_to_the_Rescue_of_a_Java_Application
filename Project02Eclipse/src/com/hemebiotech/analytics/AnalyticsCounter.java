97b3e02d4b8fcbcd16d5af1767bd8b12aad9191c
joss <joss@pc-59.home>
2020-11-21 23:32:52 +0100

    Corretion fonctionnalité

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,57 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
 	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
+	private static String root = "../Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/symptoms.txt";
+	private static String resultOut = "../Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/result.out";
+	
+	public static void main(String args[]){
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		try {
+			// first get input
+			BufferedReader reader = new BufferedReader (new FileReader(root));
+			String line = reader.readLine();
+			
+			ReadSymptomDataFromFile readSDFFile = new ReadSymptomDataFromFile(root);
+
+			ArrayList<String> result = (ArrayList) readSDFFile.GetSymptoms();
+			
+			HashMap<String, Integer> nbrCas = (HashMap<String, Integer>) readSDFFile.getCasSymptoms();
+
+			
+			StringBuilder str = new StringBuilder();
+			BufferedWriter bfwritr = new BufferedWriter( new FileWriter(resultOut));
+			
+		
+			for(Map.Entry<String, Integer> ligne: nbrCas.entrySet()) {
+				str.append(ligne.getKey() +" = " + ligne.getValue()+"\n");
+			
+			}
+			
+			 bfwritr.write(str.toString());
+			
+			 bfwritr.close();    
+		
+			
+		}catch(FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		}catch (IOException e) {
+			System.out.println(e.getMessage());
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}	
+
+		
 	}
+		
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Simple brute force implementation
@@ -13,7 +16,36 @@ import java.util.List;
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
+	private List<String> symptomes = new ArrayList<>();
+	private HashMap<String, Integer> nbrCas = new HashMap<String, Integer>();
 	
+	
+	
+	public String getFilepath() {
+		return filepath;
+	}
+
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
+	}
+
+	public List<String> getSymptomes() {
+		return symptomes;
+	}
+
+	public void setSymptomes(List<String> symptomes) {
+		symptomes = symptomes;
+	}
+
+	public HashMap<String, Integer> getNbrCas() {
+		return nbrCas;
+	}
+
+	public void setNbrCas(HashMap<String, Integer> nbrCas) {
+		this.nbrCas = nbrCas;
+	}
+
+
 	/**
 	 * 
 	 * @param filepath a full or partial path to file with symptom strings in it, one per line
@@ -22,17 +54,19 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 		this.filepath = filepath;
 	}
 	
+	/**
+	 * @return List de symptomes du fichier transmis en paramètre à la class
+	 */
 	@Override
 	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
-		
+
 		if (filepath != null) {
 			try {
 				BufferedReader reader = new BufferedReader (new FileReader(filepath));
 				String line = reader.readLine();
 				
 				while (line != null) {
-					result.add(line);
+					this.symptomes.add(line);
 					line = reader.readLine();
 				}
 				reader.close();
@@ -41,7 +75,36 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 			}
 		}
 		
-		return result;
+		Collections.sort(this.symptomes);
+		
+		return this.symptomes;
 	}
+	
+	
+	/**
+	 * @return  symptones et le nombre de cas
+	 */
+	public Map<String, Integer> getCasSymptoms(){
+	
+		int val;
+		
+			try {
+				for(String symptome: this.symptomes) {
+					
+					if(this.nbrCas.containsKey(symptome)) {
+						val = this.nbrCas.get(symptome);
+						val +=1;
+						this.nbrCas.put(symptome, val);
+						
+					}else {
+						this.nbrCas.put(symptome, 1);
+					}
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 
+		return this.nbrCas;
+	}
 }
